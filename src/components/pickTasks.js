290493f01c1b71d82d0872c0859964f2707c92e4
joe <joe.ulica@gmail.com>
2023-02-16 14:37:27 -0500

    build out and do initial styling of pickTasks component

--- src/components/pickTasks.js
+++ src/components/pickTasks.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './pickTasks.css'
 
 export default function PickTasks() {
   const [tasks, setTasks] = useState(null)
@@ -8,16 +9,31 @@ export default function PickTasks() {
   }
 
   return (
-    <div className="pickTasks">
-      <h1 className="pickTasks-Title">What are 3 - 5 things you want to get done today?</h1>
-      <form onSubmit={handleSubmit()}>
-        <button type="button" className="btn btn-primary">Primary</button>
-        <button type="button" className="btn btn-success">Success</button>
-        <input type="text"></input>
-        <input type="text"></input>
-        <input type="text"></input>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <div className="pickTasks-background">
+      <section className="pickTasks-main">
+        <h1 className="pickTasks-Title">What do you want to get done today?</h1>
+        <form onSubmit={handleSubmit()}>
+          <div id="pickTasks-container">
+            <div>
+              <h3 style={{ marginLeft: '70%' }}>Task</h3>
+              <ol>
+                <li><input type="text" className="task-input"></input></li>
+                <li><input type="text" className="task-input"></input></li>
+                <li><input type="text" className="task-input"></input></li>
+              </ol>
+            </div>
+            <div id="estimates">
+              <h3>Time Estimate (hours)</h3>
+              <li className="time-estimate-list"><input type="text" className="time-estimate-input"></input></li>
+              <li className="time-estimate-list"><input type="text" className="time-estimate-input"></input></li>
+              <li className="time-estimate-list"><input type="text" className="time-estimate-input"></input></li>
+            </div>
+          </div>
+          <button className="add">Add more</button> {/* add functionality to add additional tasks  */}
+          <button className="close" type="submit">Submit</button>
+        </form >
+      </section>
+    </div >
   )
 }
+
